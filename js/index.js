9fa0bf929bdd8e9e494737f1978dac3815a98d2a
TirumaniNadar <111589721+TirumaniNadar@users.noreply.github.com>
2025-05-13 00:11:29 +0530

    meta fixes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const scrollBtn = document.querySelector('.scrolltotop');
+const textTypingJs = document.querySelector('#text_typing_js');
 
 window.addEventListener('scroll', function () {
     if (window.scrollY > 200) {
@@ -29,3 +30,21 @@ const observer = new IntersectionObserver((entries, obs) => {
 document.querySelectorAll('.cust-animation-div').forEach(card => {
     observer.observe(card);
 });
+
+const isMobile = window.innerWidth <= 768;
+const text = "Tirumani Nadar";
+let i = 0;
+textTypingJs.innerHTML = '';
+function printText() {
+    if (i < text.length) {
+        textTypingJs.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(printText, 250);
+    }
+}
+
+if (!isMobile) {
+    printText();
+} else {
+    textTypingJs.innerHTML = text;
+}
